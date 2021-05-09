e8e47149db843e7d342ee3f98e0f3c82d5f981ed
Kurt Woodward <kurt.woodward@outlook.com>
2021-05-08 21:53:25 -0700

    Update with authentication, authorization, containerization w/ Docker

--- SportlandSports/Startup.cs
+++ SportlandSports/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.EntityFrameworkCore;
 using SportlandSports.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace SportlandSports {
     public class Startup {
@@ -34,14 +35,32 @@ namespace SportlandSports {
             services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
             services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
             services.AddServerSideBlazor();
+
+            services.AddDbContext<AppIdentityDbContext>(options => options.UseSqlServer(Configuration["ConnectionStrings:IdentityConnection"]));
+            services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppIdentityDbContext>();
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+
+            if (env.IsProduction())
+            {
+                app.UseExceptionHandler("/error");
+            }
+            else
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseStatusCodePages();
+            }
+
             app.UseDeveloperExceptionPage();
             app.UseStatusCodePages();
             app.UseStaticFiles();
             app.UseSession();
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllerRoute("catpage",
                     "{category}/Page{productPage:int}",
@@ -63,6 +82,7 @@ namespace SportlandSports {
             });
 
             SeedData.EnsurePopulated(app);
+            IdentitySeedData.EnsurePopulated(app);
         }
     }
 }
